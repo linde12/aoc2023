d34b0720b351aae8e39ba09ad33c1e45709f31cc
linde12 <lindeoscar@gmail.com>
2023-12-01 18:01:40 +0100

    day01

--- rust/day01/src/bin/part01.rs
+++ rust/day01/src/bin/part01.rs
@@ -7,10 +7,30 @@ fn main() {
 }
 
 fn process(input: &str) -> Result<String> {
-    Ok(input.into())
+    let sum: usize = input
+        .lines()
+        .map(|l| {
+            let digit1 = l.chars().skip_while(|c| !c.is_digit(10)).next().unwrap();
+            let digit2 = l
+                .chars()
+                .rev()
+                .skip_while(|c| !c.is_digit(10))
+                .next()
+                .unwrap();
+
+            let s = format!("{}{}", digit1, digit2);
+            s.parse::<usize>().unwrap()
+        })
+        .sum();
+
+    Ok(sum.to_string())
 }
 
 #[test]
 fn it_works() {
-    assert!(1 == 1);
+    let input = r#"1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet"#;
+    assert_eq!(process(input).unwrap(), "142".to_owned());
 }
--- rust/day01/src/bin/part02.rs
+++ rust/day01/src/bin/part02.rs
@@ -1,16 +1,60 @@
+use std::collections::HashMap;
+
 use anyhow::Result;
 
 fn main() {
-    let input = include_str!("./input01.txt");
+    let input = include_str!("./input02.txt");
     let output = process(input);
     dbg!(output.unwrap());
 }
 
+const DIGITS: [&str; 9] = [
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+];
+
+fn find_first_digit(bytes: &[u8], iterable: impl IntoIterator<Item = usize>) -> usize {
+    iterable
+        .into_iter()
+        .find_map(|i| {
+            // char at index i is a digit (1,2,3,4) - return the digit
+            if bytes[i].is_ascii_digit() {
+                return Some((bytes[i] as char).to_digit(10).unwrap() as usize);
+            }
+
+            // see if the remaining characters starts with one of the string digits
+            for (value, digit) in DIGITS.into_iter().enumerate() {
+                if bytes[i..].starts_with(digit.as_bytes()) {
+                    return Some(value + 1);
+                }
+            }
+            None
+        })
+        .unwrap()
+}
+
 fn process(input: &str) -> Result<String> {
-    Ok(input.into())
+    let sum: usize = input
+        .lines()
+        .map(|l| {
+            let bytes = l.as_bytes();
+            let digit1 = 10 * find_first_digit(bytes, 0..bytes.len());
+            let digit2 = find_first_digit(bytes, (0..bytes.len()).rev());
+
+            digit1 + digit2
+        })
+        .sum();
+
+    Ok(sum.to_string())
 }
 
 #[test]
 fn it_works() {
-    assert!(1 == 1);
+    let input = r#"two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen"#;
+    assert_eq!(process(input).unwrap(), "281".to_owned());
 }
